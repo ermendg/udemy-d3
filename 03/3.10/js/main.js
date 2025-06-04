dce1f57c5e5772670c50882a42da792ebea271e3
ermendg <43694534+ermendg@users.noreply.github.com>
2025-06-04 22:28:48 +0900

    Enhance bar charts

--- 03/3.10/js/main.js
+++ 03/3.10/js/main.js
@@ -22,7 +22,7 @@ g.append("text")
   .attr("y", HEIGHT + 110)
   .attr("font-size", "20px")
   .attr("text-anchor", "middle")
-  .text("The word's tallest buildings")
+  .text("The world's tallest buildings")
 
 // Y label
 g.append("text")
@@ -47,7 +47,7 @@ d3.json("data/buildings.json").then(data => {
   
   const y = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.height)])
-    .range([0, HEIGHT])
+    .range([HEIGHT, 0])
 
   const xAxisCall = d3.axisBottom(x)
   g.append("g")
@@ -71,9 +71,9 @@ d3.json("data/buildings.json").then(data => {
     .data(data)
   
   rects.enter().append("rect")
-    .attr("y", 0)
+    .attr("y", d => y(d.height))
     .attr("x", (d) => x(d.name))
     .attr("width", x.bandwidth)
-    .attr("height", d => y(d.height))
+    .attr("height", d => HEIGHT - y(d.height))
     .attr("fill", "grey")
 })
